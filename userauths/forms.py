d5b99979a4e10fcc2164ac5453c7948aacf3a744
Philip Obiri <philipobiri3.1@gmail.com>
2023-09-23 11:10:25 +0000

    Init Commit

--- userauths/forms.py
+++ userauths/forms.py
@@ -2,7 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from userauths.models import User
 
-class UserRegisterForm(UserCreationForm):
-    class Meta :
-        model = User
-        fields = ['username', 'email', 'password1','password2']    
+#User Registration Form
+# class UserRegisterForm(UserCreationForm):
+#     class Meta :
+#         model = User
+#         fields = ['username', 'email', 'password1','password2']    
