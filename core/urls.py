03082677ab0502ff067602ae9eaa3c83d9ce9b4d
Philip Obiri <philipobiri3.1@gmail.com>
2023-10-05 23:53:57 +0000

    Initial Commit

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from . import views 
+from core import views
+# from core import views, transfer, transaction, payment_request, credit_card
 
-app_name= "core"
+
+app_name = "core"
+
 urlpatterns = [
     path("", views.index, name="index"),
 ]
+
