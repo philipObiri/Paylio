88b15be0945c7fd2114bfa138f2d23894455d6b7
Philip Obiri <philipobiri3.1@gmail.com>
2023-10-06 00:03:04 +0000

    Add User Registration Forms

--- userauths/forms.py
+++ userauths/forms.py
@@ -4,6 +4,10 @@ from userauths.models import User
 
 #User Registration Form
 class UserRegisterForm(UserCreationForm):
-    class Meta :
+    username = forms.CharField(widget=forms.TextInput(attrs={"placeholder":"Username"}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={"placeholder":"Email"}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Password"}))
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={"placeholder":"Confirm Password"}))
+    class Meta:
         model = User
-        fields = ['username', 'email', 'password1','password2']    
+        fields = ['username', 'email', 'password1', 'password2']    
